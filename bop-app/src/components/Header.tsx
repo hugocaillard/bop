0804f9fe43a55376e9e7a628748779fe55e3de64
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2023-03-19 20:01:25 +0100

    feat: nav

--- bop-app/src/components/Header.tsx
+++ bop-app/src/components/Header.tsx
@@ -4,13 +4,26 @@ import { H1 } from "./ui/Headings";
 
 export function Header() {
   return (
-    <>
+    <div className="mb-6">
       <H1>
         <Link to="/">bop command builder</Link>
       </H1>
       <p className="font-bold text-red-800">
         This is a proof of concept, not even in alpha. Use at your own risks
       </p>
-    </>
+      <Link
+        className="text-amber-600 transition-colors hover:text-amber-700 hover:underline"
+        to="/"
+      >
+        Home
+      </Link>
+      {" - "}
+      <Link
+        className="text-amber-600 transition-colors hover:text-amber-700 hover:underline"
+        to="/bft"
+      >
+        BFT
+      </Link>
+    </div>
   );
 }
